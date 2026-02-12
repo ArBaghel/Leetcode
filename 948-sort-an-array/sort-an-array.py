1f3a5121e535283647f2c37ef07c846a028f608e
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-02-12 13:34:45 +0530

    Time: 618 ms (60.73%) | Memory: 28.7 MB (27.12%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # Bubble Sort
+        # for i in range(len(nums)):
+        #     for j in range (len(nums)-1):
+        #         if nums[j]>=nums[j+1]:
+        #             nums[j],nums[j+1]=nums[j+1],nums[j]
+        # return nums  
+        #return sorted(nums)
+        if len(nums)<=1:
+            return nums
+        mid=len(nums)//2
+        l=self.sortArray(nums[:mid])
+        r=self.sortArray(nums[mid:])
+        return self.merge(l,r)
+    def merge(self,l,r):
+        res=[]
+        i=j=0
+        while i<len(l) and j<len(r):
+            if l[i]<=r[j]:
+                res.append(l[i])
+                i+=1
+            else :
+                res.append(r[j])
+                j+=1
+        res.extend(l[i:])
+        res.extend(r[j:])
+        return res
+
+        
+
