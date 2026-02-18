66c4f6e719138f47b0c591e3a9f0dd8ad3e06fa6
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-02-18 23:12:34 +0530

    Time: 641 ms (45.71%) | Memory: 22.5 MB (25.16%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -17,9 +17,9 @@ class Solution:
                     res.append([nums[i], nums[j], nums[k]])
                     j += 1
                     k -= 1
-                    while j < k and nums[j] == nums[j - 1]:
+                    while j<k and nums[j] == nums[j - 1]:
                         j += 1
-                    while j < k and nums[k] == nums[k + 1]:
+                    while j<k and nums[k] == nums[k + 1]:
                         k -= 1
         return res
 
