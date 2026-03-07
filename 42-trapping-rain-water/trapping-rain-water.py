e0662dd7269e7f82b5ea3dd8c600d9e5ad49bbf1
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-08 00:42:30 +0530

    Time: 11 ms (47.09%) | Memory: 21.1 MB (77.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,15 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         i,j=0,len(height)-1
-        leftMax,rightMax=height[0],height[j]
+        leftMax,rightMax=height[i],height[j]
         water=0
         while i<j:
             if leftMax<=rightMax:
-                water+=leftMax-height[i]
                 i+=1
                 leftMax=max(leftMax,height[i])
+                water+=leftMax-height[i]
+                
+               
             elif rightMax<=leftMax:
-                water+=rightMax-height[j]
                 j-=1
                 rightMax=max(rightMax,height[j])
+                water+=rightMax-height[j]
+                
+               
         return water
