9ace27f88c3aa65e9287883f0c8860194d935e65
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-10 10:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (75.28%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,6 +3,6 @@ class Solution:
      merged=sorted(nums1+nums2)
      n=len(merged)
      if n %2==1:
-        return float(merged[n // 2])
+        return (merged[n //2])
      else:
         return (merged[n // 2 - 1] + merged[n//2]) / 2.0
