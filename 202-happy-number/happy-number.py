6cabbf56d7af3a080cc3583b9ab6a5aef8f05de6
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-14 21:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (22.51%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if (n==1 or n==7):
+        if n==1 or n==7:
             return True
-        elif (n<10):
+        elif n<10:
             return False
-        else:
-            sum=0
-            while (n>0):
-                digit=n%10
-                sum+=digit*digit
-                n//=10
-            return self.isHappy(sum)
-        
 
-        
+        else:
+            sum1=0
+
+            while n>0:
+                digit=n%10
+                sum1+=digit**2
+                n//=10
+            return self.isHappy(sum1)
