f8fef13ed9e8cd50431fb9e8adf6aa9ccb20cd1a
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-24 11:40:21 +0530

    Time: 7 ms (85.64%) | Memory: 21.6 MB (51.88%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,22 +1,22 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        # m,n=len(mat),len(mat[0])
-        # res=[]
-        # for d in range(m+n-1):
-        #     diag=[]
-        #     r=max(0,d-n+1)
-        #     c=d-r
-        #     while r< m and c>=0:
-        #         diag.append(mat[r][c])
-        #         r,c=r+1,c-1
-        #     res.extend(diag[::-1] if d %2 ==0 else diag)
-        # return res
-        m, n = len(mat), len(mat[0])
-        res = []
-        for d in range(m + n - 1):
-            tmp = [mat[i][d - i] for i in range(max(0, d - n + 1), min(m, d + 1))]
-            res.extend(tmp[::-1] if d % 2 == 0 else tmp)
+        m,n=len(mat),len(mat[0])
+        res=[]
+        for d in range(m+n-1):
+            diag=[]
+            r=max(0,d-n+1)
+            c=d-r
+            while r< m and c>=0:
+                diag.append(mat[r][c])
+                r,c=r+1,c-1
+            res.extend(diag[::-1] if d %2 ==0 else diag)
         return res
+        # m, n = len(mat), len(mat[0])
+        # res = []
+        # for d in range(m + n - 1):
+        #     tmp = [mat[i][d - i] for i in range(max(0, d - n + 1), min(m, d + 1))]
+        #     res.extend(tmp[::-1] if d % 2 == 0 else tmp)
+        # return res
       
         
 
