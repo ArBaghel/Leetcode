1a53f1cc073ce33756e569964d19bb7ca1ca867c
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-24 13:30:01 +0530

    Time: 14 ms (41.30%) | Memory: 21.5 MB (92.08%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -2,14 +2,14 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         m,n=len(mat),len(mat[0])
         res=[]
-        for d in range(m+n-1):
-            diag=[]
-            r=max(0,d-n+1)
-            c=d-r
-            while r< m and c>=0:
-                diag.append(mat[r][c])
-                r,c=r+1,c-1
-            res.extend(diag[::-1] if d %2 ==0 else diag)
+        for d in range( m+n-1 ):
+            diag = []
+            r = max(0,d-n+1)
+            c = d - r
+            while r < m and c >= 0:
+                diag.append ( mat [r] [c] )
+                r,c = r + 1,c - 1
+            res.extend ( diag [ ::-1 ] if d % 2 == 0 else diag )
         return res
         # m, n = len(mat), len(mat[0])
         # res = []
