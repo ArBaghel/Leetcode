0edbb264ea9337018cc201c0c13228b0c33aaf77
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-03-24 10:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (97.66%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,40 +1,37 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        n=len(matrix)
-        m=len(matrix[0])
-
-        total=m*n 
-        ans=[] 
-        count=0
-
-        cols,rows=0,0
-        colend,rowend=m-1,n-1
-
-        while count < total :
-            #rows , cols->colend
-            for i in range(cols,colend+1):
-                ans.append(matrix[rows][i])
-                count+=1
-            rows+=1
-            if count==total:
+        n,m=len(matrix),len(matrix[0])
+        total=m*n
+        ans=[]
+        c=0
+        row,col=0,0
+        rowe,cole=n-1,m-1
+        while c < total:
+            # row, col -> cole
+            for i in range (col,cole+1):
+                ans.append(matrix[row][i])
+                c+=1
+            row+=1
+            if c==total:
                 break
-            #colend , rows->rowend
-            for i in range(rows,rowend+1):
-                ans.append(matrix[i][colend])
-                count+=1
-            colend-=1
-            if count==total:
+             # cole, row -> rowe
+            for i in range (row,rowe+1):
+                ans.append(matrix[i][cole])
+                c+=1
+            cole -= 1
+            if c==total:
                 break
-            #rowend , colend->cols
-            for i in range(colend,cols-1,-1):
-                ans.append(matrix[rowend][i])
-                count+=1
-            rowend-=1
-            if count==total:
+             # rowe, cole -> col
+            for i in range (cole,col-1,-1):
+                ans.append(matrix[rowe][i])
+                c+=1
+            rowe-=1
+            if c==total:
                 break
-            #cols , rowend->rows
-            for i in range(rowend,rows-1,-1):
-                ans.append(matrix[i][cols])
-                count+=1
-            cols+=1
+            # cole , rowe -> row
+            for i in range( rowe, row-1,-1):
+                ans.append(matrix[i][col])
+                c+=1
+            col+=1
         return ans
+
