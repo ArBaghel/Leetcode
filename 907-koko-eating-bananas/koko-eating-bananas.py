f1f06c45a1b36ad41b4e14ff83fbbc0d46af02f5
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-04-19 23:02:20 +0530

    Time: 423 ms (5.13%) | Memory: 20.7 MB (18.20%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,16 +1,24 @@
 class Solution:
-    def gethour(self,piles,mid):
-        ans=0
-        for pile in piles:
-            ans+=(pile+mid-1)//mid
-        return ans
+    # def gethour(self,piles,mid):
+    #     ans=0
+    #     for pile in piles:
+    #         ans+=(pile+mid-1)//mid
+    #     return ans
+    # def minEatingSpeed(self, piles: List[int], h: int) -> int:
+    #     l,r=1,max(piles)
+    #     while l<=r:
+    #         mid=(l+r)//2
+    #         if self.gethour(piles,mid) > h:
+    #             l=mid+1
+    #         else:
+    #             r=mid-1
+    #     return l
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l,r=1,max(piles)
-        while l<=r:
-            mid=(l+r)//2
-            if self.gethour(piles,mid) > h:
+        while l<r:
+            mid =(l+r)//2
+            if sum((pile+mid-1)//mid for pile in piles) > h:
                 l=mid+1
             else:
-                r=mid-1
+                r=mid
         return l
-        
