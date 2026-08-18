7a923f39685759455464f415d192d301997dc8a5
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-08-18 23:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (39.00%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -9,6 +9,6 @@ class Solution:
         def dfs(node):
             if not node: return 0
             l, r = dfs(node.left), dfs(node.right)
-            if l == -1 or r == -1 or abs(l - r) > 1: return -1
+            if  l==-1 or r==-1 or abs(l - r) > 1: return -1
             return max(l, r) + 1
-        return dfs(root) != -1
+        return dfs(root) !=-1
