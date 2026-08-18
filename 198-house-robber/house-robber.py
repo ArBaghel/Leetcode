261f4cb36aaf1ab023cb7c5f88ac3b1bd166519e
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-08-18 19:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.03%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,10 @@
+from functools import lru_cache
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        p1=p2=0
-        for i in nums:
-            p1,p2=max(p2+i,p1),p1
-        return p1
-        
-
-        
+   def rob(self, nums: List[int]) -> int:
+    n = len(nums)
+    @lru_cache(None)
+    def solve(i):
+        if i>=n:return 0
+        return max(nums[i]+solve(i+2),solve(i+1))
+    return solve(0)
+   
