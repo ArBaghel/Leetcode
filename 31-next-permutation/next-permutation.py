a27f4675fb238e7f31e6cfa9c4fafac45e4c3a00
Aditya Singh Baghel <arbaghel.rb@gmail.com>
2026-09-25 22:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.82%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,53 +1,19 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pivot=-1
-        for i in range(len(nums)-2,-1,-1):
+        p=-1
+        for i in range (len(nums)-2,-1,-1):
             if nums[i]<nums[i+1]:
-                pivot =i
+                p=i
                 break
-        if pivot ==-1:
+        if p==-1:
             return nums.reverse()
         for i in range (len(nums)-1,-1,-1):
-            if nums[i]>nums[pivot]:
-                nums[i],nums[pivot]=nums[pivot],nums[i]
+            if nums[i]>nums[p]:
+                nums[i],nums[p]=nums[p],nums[i]
                 break
-        nums[pivot+1:]=reversed(nums[pivot+1:])
-            
+        nums[p+1:]=reversed(nums[p+1:])
 
-
-#   class Solution {
-# public:
-#     void nextPermutation(vector<int>& nums) {
-
-#         int n = nums.size();
-
-#         // Find pivot
-#         int pivot = -1;
-#         for (int i = n - 2; i >= 0; i--) {
-#             if (nums[i] < nums[i + 1]) {
-#                 pivot = i;
-#                 break;
-#             }
-#         }
-
-#         // If no pivot, reverse entire array
-#         if (pivot == -1) {
-#             reverse(nums.begin(), nums.end());
-#             return;
-#         }
-
-#         // Find next greater element
-#         for (int i = n - 1; i > pivot; i--) {
-#             if (nums[i] > nums[pivot]) {
-#                 swap(nums[i], nums[pivot]);
-#                 break;
-#             }
-#         }
-
-#         // Reverse remaining part
-#         reverse(nums.begin() + pivot + 1, nums.end());
-#     }
-# };
+        
